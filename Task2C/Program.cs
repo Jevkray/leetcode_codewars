bc71c335ee1e354651117062855cb8f7057c11a3
Евгений Красовский <semmitwinsboss@gmail.com>
2023-03-31 01:52:42 +0300

    It still works, mb

---
+++ Task2C/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Codewars1
+{
+    public static void Main()
+    {
+        Console.WriteLine(Kata.Maskify("asdsfadgfhsgbvcncxbg354etra"));
+    }
+
+    public static class Kata
+    {
+        public static string Maskify(string cc)
+        {
+            for (int i = 0; i < cc.Length - 4; i++)
+            {
+                cc = cc.Remove(i, 1).Insert(i, "#");
+            }
+            return cc;
+        }
+    }
+
+}
+
+/*public static class Kata
+{
+  // return masked string
+  public static string Maskify(string cc)
+  {
+    if (cc.Length < 4)
+    return cc;
+    else return cc.Substring(cc.Length-4).PadLeft(cc.Length, '#');
+  }
+}*/
