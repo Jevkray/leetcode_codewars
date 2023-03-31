2d0b698416433362b3f6efd1ab86459abc85c52b
Евгений Красовский <semmitwinsboss@gmail.com>
2023-03-31 11:57:16 +0300

    -

--- leetcode_template/Program.cs
+++ leetcode_template/Program.cs
@@ -2,24 +2,7 @@
 {
     public static void Main()
     {
-        int[] numbers = { 3, 2, 2, 3 };
-        Console.WriteLine(Solution.RemoveElement( numbers , 3));
-    }
 
-    public static class Solution
-    {
-        public static int RemoveElement(int[] nums, int val)
-        {
-            int k = 0;
-            foreach (int c in nums)
-            {
-                if (val == c)
-                {
-                    k++;
-                }
-            }
-            return k;
-        }
     }
 
 }
