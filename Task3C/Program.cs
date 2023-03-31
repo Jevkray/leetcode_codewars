cceeccaae681b0440027502ad8a375f110c813f6
Евгений Красовский <semmitwinsboss@gmail.com>
2023-03-31 04:04:26 +0300

    Task3 CodeWars - Check for the presence of certain characters simultaneously in the string ( Проверить на наличие определенных символов одновременнов строке )

---
+++ Task3C/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Codewars1
+{
+    public static void Main()
+    {
+        Console.WriteLine(Kata.IsPangram("A pangram is a sentence that contains every single letter of the alphabet at least once.  "));
+    }
+
+
+    public static class Kata
+    {
+        public static bool IsPangram(string str)
+        {
+            char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            List<char> list = new List<char>(charArray);
+            var strLower = str.ToLower();
+            foreach (char c in strLower)
+            {
+                if (list.Contains(c))
+                {
+                    list.Remove(c);
+                    strLower = strLower.Replace(c.ToString(), "");
+                }
+            }
+            if (list.Count == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+
+}
