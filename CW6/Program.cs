55e23f6281305250694c9466fd669bf498cadd42
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-02 14:32:15 +0300

    CW 6

--- CW6/Program.cs
+++ CW6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+using System;
+
+public class LeetCodeTask
+{
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(", ", (WhichAreIn.inArray(new string[] { "cod", "code", "wars", "ewar" }, new string[] { "lively", "alive", "harp", "sharp", "armstrong", "codewars" }))));
+    }
+}
+
+public class WhichAreIn
+{
+    public static string[] inArray(string[] array1, string[] array2)
+    {
+        var x = array1.Where(s1 => array2.Any(s2 => s2.Contains(s1))).OrderBy(s => s).ToArray();
+        return x;
+    }
+}
+
+//Вывод один- учить LINQ
