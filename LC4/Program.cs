fa83a9a50f270140904f13edbe2c69c15179b0e5
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-04 02:16:41 +0300

    save lc4

---
+++ LC4/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Solution.LengthOfLongestSubstring("dvdf"));
+    }
+
+    public class Solution
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            string sub = "";
+            string currsub = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!(currsub.Contains(s[i])))
+                {
+                    currsub += s[i];
+                    if (sub.Length <= currsub.Length)
+                    {
+                        sub = currsub;
+                    }
+                }
+                else if (currsub.Contains(s[i]))
+                {
+                    currsub = s[i].ToString();
+                }
+            }
+            return sub.Length;
+        }
+    }
+}
+
