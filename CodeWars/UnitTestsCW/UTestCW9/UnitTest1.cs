d35b03526a0938c42ceb90c7db39cbaff69fcb66
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-05 01:57:12 +0300

    CW9 add

---
+++ CodeWars/UnitTestsCW/UTestCW9/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UTestCW9
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
