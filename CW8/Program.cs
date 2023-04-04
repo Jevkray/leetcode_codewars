1c48696b7ec4f7c78f50c9246498551240e00232
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-05 00:26:17 +0300

    CW8 and unit tests cw 8 - done

--- CW8/Program.cs
+++ CW8/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class CWTask
+{
+    public static void Main()
+    {
+        Console.WriteLine(Kata.IsIsogram("moOse"));
+    }
+}
+
+public class Kata
+{
+    public static bool IsIsogram(string str)
+    {
+        string subStr = "";
+        str = str.ToLower();
+        foreach (char s in str)
+        {
+            if (!(subStr.Contains(s)))
+            {
+                subStr += s;
+            }
+            else if (subStr.Contains(s))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ TestProject1/TwstCW8.cs
@@ -0,0 +1,25 @@
+﻿namespace TestCW8
+{
+    [TestFixture]
+    public class BasicTests
+    {
+
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Dermatoglyphics").Returns(true);
+                yield return new TestCaseData("isogram").Returns(true);
+                yield return new TestCaseData("moose").Returns(false);
+                yield return new TestCaseData("isIsogram").Returns(false);
+                yield return new TestCaseData("aba").Returns(false);
+                yield return new TestCaseData("moOse").Returns(false);
+                yield return new TestCaseData("thumbscrewjapingly").Returns(true);
+                yield return new TestCaseData("").Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => Kata.IsIsogram(str);
+    }
+}
