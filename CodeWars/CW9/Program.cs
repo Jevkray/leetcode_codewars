3cee728c3ae0827ae6e75276be5ba6eb9068cea3
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-05 23:05:48 +0300

    CW 9 done

--- CodeWars/CW9/Program.cs
+++ CodeWars/CW9/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CWTask
+{
+    public static void Main()
+    {
+        Console.WriteLine(Kata.OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
+    }
+}
+public class Kata
+{
+    public static IEnumerable<string> OpenOrSenior(int[][] data)
+    {
+        IEnumerable<string> status = new string[] { };
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            bool statusAge = false;
+            bool statusHandicap = false;
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                if ( j == 0)
+                {
+                    if ((data[i][j] >= 55))
+                    {
+                        statusAge = true;
+                    }
+                    else statusAge = false;
+                }
+                else if (j == 1)
+                {
+                    if ((data[i][j] <= 26) && (data[i][j] >= -2))
+                    {
+                        if ((data[i][j] > 7))
+                        {
+                            statusHandicap = true;
+                        }
+                        else statusHandicap = false;
+                    }
+                    else return new string[] { };
+                }
+                else return new string[] { };
+     
+            }
+
+            if (statusAge == true && statusHandicap == true)
+            {
+                status = status.Concat(new[] { "Senior" });
+            }
+            else if (statusAge == false || statusHandicap == false)
+            {
+                status = status.Concat(new[] { "Open" });
+            }
+        }
+        return status;
+    }
+}
--- CodeWars/UnitTestsCW/UTestCW9/UnitTest1.cs
+++ CodeWars/UnitTestsCW/UTestCW9/UnitTest1.cs
@@ -1,16 +1,19 @@
-namespace UTestCW9
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
 {
-    public class Tests
+    [TestFixture]
+    public class KataOpenOrSeniorTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void SampleTest()
         {
+            Assert.AreEqual(new[] { "Open", "Senior", "Open", "Senior" }, Kata.OpenOrSenior(new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } }));
+            Assert.AreEqual(new[] { "Open", "Open", "Open", "Open" }, Kata.OpenOrSenior(new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } }));
+            Assert.AreEqual(new[] { "Senior", "Open", "Open", "Open" }, Kata.OpenOrSenior(new[] { new[] { 59, 12 }, new[] { 45, 21 }, new[] { -12, -2 }, new[] { 12, 12 } }));
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
     }
 }
