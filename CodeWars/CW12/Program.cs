922a9057f2d375ee07f329db018c24c0d40b1e09
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-08 17:00:41 +0300

    CW12 done Kata 4
    Write two functions that convert a roman numeral to and from an integer value. Multiple roman numeral values will be tested for each function.
    
    Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
    
    Input range : 1 <= n < 4000
    
    In this kata 4 should be represented as IV, NOT as IIII (the "watchmaker's four").

--- CodeWars/CW12/Program.cs
+++ CodeWars/CW12/Program.cs
@@ -7,7 +7,7 @@ public class CWTask
 {
     public static void Main()
     {
-        Console.WriteLine(RomanNumerals.FromRoman("XI"));
+        Console.WriteLine(RomanNumerals.ToRoman(59));
     }
 }
 
@@ -15,9 +15,83 @@ public class RomanNumerals
 {
     public static string ToRoman(int n)
     {
-        return "";
+        if ((n > 0) && (n <= 4000))
+        {
+            string romanString = string.Empty;
+
+            while (n > 0)
+            {
+                if (n >= 1000)
+                {
+                    n = n - 1000;
+                    romanString += "M";
+                }
+                else if (n >= 900)
+                {
+                    n = n - 900;
+                    romanString += "CM";
+                }
+                else if (n >= 500)
+                {
+                    n = n - 500;
+                    romanString += "D";
+                }
+                else if (n >= 400)
+                {
+                    n = n - 400;
+                    romanString += "CD";
+                }
+                else if (n >= 100)
+                {
+                    n = n - 100;
+                    romanString += "C";
+                }
+                else if (n >= 90)
+                {
+                    n = n - 90;
+                    romanString += "XC";
+                }
+                else if (n >= 50)
+                {
+                    n = n - 50;
+                    romanString += "L";
+                }
+                else if (n >= 40)
+                {
+                    n = n - 40;
+                    romanString += "XL";
+                }
+                else if (n >= 10)
+                {
+                    n = n - 10;
+                    romanString += "X";
+                }
+                else if (n >= 9)
+                {
+                    n = n - 9;
+                    romanString += "IX";
+                }
+                else if (n >= 5)
+                {
+                    n = n - 5;
+                    romanString += "V";
+                }
+                else if (n >= 4)
+                {
+                    n = n - 4;
+                    romanString += "IV";
+                }
+                else if (n >= 1)
+                {
+                    n = n - 1;
+                    romanString += "I";
+                }
+            }
+            return romanString;
+        }
+        else return "";
     }
-    ////////////////////////////////////////////////////////////////
+
     public static int FromRoman(string input)
     {
         int output = 0;
