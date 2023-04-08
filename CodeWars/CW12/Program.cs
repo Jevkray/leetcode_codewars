0baf09bf7e3660596e8bc2505240f03d41710f0c
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-08 05:00:56 +0300

    CW12 - Half
    Write two functions that convert a roman numeral to and from an integer value. Multiple roman numeral values will be tested for each function.
    
    Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.

---
+++ CodeWars/CW12/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+public class CWTask
+{
+    public static void Main()
+    {
+        Console.WriteLine(RomanNumerals.FromRoman("XI"));
+    }
+}
+
+public class RomanNumerals
+{
+    public static string ToRoman(int n)
+    {
+        return "";
+    }
+    ////////////////////////////////////////////////////////////////
+    public static int FromRoman(string input)
+    {
+        int output = 0;
+
+        foreach (char symbolInput in input)
+        {
+            output = checkFromRomanSymbol(symbolInput, output);
+        }
+        int minusOutput = checkComboFromRomanSymbol(input);
+
+        return (output + minusOutput);
+    }
+
+    public static int checkFromRomanSymbol(char symbol, int output)
+    {
+        int value = 0;
+        if (symbol == 'I')
+        {
+            value = 1;
+        }
+        if (symbol == 'V')
+        {
+            value = 5;
+        }
+        if (symbol == 'X')
+        {
+            value = 10;
+        }
+        if (symbol == 'L')
+        {
+            value = 50;
+        }
+        if (symbol == 'C')
+        {
+            value = 100;
+        }
+        if (symbol == 'D')
+        {
+            value = 500;
+        }
+        if (symbol == 'M')
+        {
+            value = 1000;
+        }
+        return (output + value);
+    }
+
+    public static int checkComboFromRomanSymbol(string input)
+    {
+        int value = 0;
+        if ((input.Contains("IV")) || (input.Contains("IX")))
+        {
+            value -= 2;
+        }
+        if ((input.Contains("XL")) || (input.Contains("XC")))
+        {
+            value -= 20;
+        }
+        if ((input.Contains("CD")) || (input.Contains("CM")))
+        {
+            value -= 200;
+        }
+        return (value);
+    }
+}
---
+++ CodeWars/UnitTestsCW/UTestCW12/UnitTest1.cs
@@ -0,0 +1,55 @@
+namespace Solution
+{
+    using NUnit.Framework;
+    using System;
+
+    // TODO: Replace examples and use TDD by writing your own tests
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void TestToRoman_001()
+        {
+            int input = 1;
+            string expected = "I";
+
+            string actual = RomanNumerals.ToRoman(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestToRoman_002()
+        {
+            int input = 2;
+            string expected = "II";
+
+            string actual = RomanNumerals.ToRoman(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestFromRoman_001()
+        {
+            string input = "I";
+            int expected = 1;
+
+            int actual = RomanNumerals.FromRoman(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestFromRoman_002()
+        {
+            string input = "II";
+            int expected = 2;
+
+            int actual = RomanNumerals.FromRoman(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
