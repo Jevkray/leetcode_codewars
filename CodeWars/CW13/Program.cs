254ae1124c710ea89aa414bccc27ad27551de024
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-09 22:17:59 +0300

    CW13 Done.
    Convert number to reversed array of digits
    Given a random non-negative number, you have to return the digits of this number within an array in reverse order.

---
+++ CodeWars/CW13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    public class CWTask
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Digitizer.Digitize(52341));
+        }
+    }
+    public class Digitizer
+    {
+        public static long[] Digitize(long n)
+        {
+            long[] nArray = new long[] { };
+            foreach (char c in n.ToString().ToArray().Reverse())
+            {
+                Array.Resize(ref nArray, nArray.Length + 1);
+                nArray[nArray.Length - 1] = Convert.ToInt64(c.ToString());
+            }
+            return nArray;
+        }
+    }
+}
