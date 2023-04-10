52d68785c581e3c869383b5031d8b9cbee65e20b
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-10 23:41:12 +0300

    CW14

---
+++ CodeWars/CW14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Solution
+{
+    public class CWTask
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Kata.IsPrime(52341));
+        }
+    }
+
+    public static class Kata
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Convert.ToInt32(Math.Sqrt(n)); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
