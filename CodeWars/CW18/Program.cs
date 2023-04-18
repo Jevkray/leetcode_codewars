c228753c76696e73c2eac2b26e3f7a83802ac802
Евгений Красовский <semmitwinsboss@gmail.com>
2023-04-18 12:52:58 +0300

    CW18 without test / do not need

---
+++ CodeWars/CW18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Solution
+{
+    public class CWTask
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Kata.XO("xxoo"));
+        }
+    }
+    public class Kata
+    {
+        public static bool XO(string input)
+        {
+            int xCount = 0;
+            int oCount = 0;
+            input = input.ToLower();
+            foreach (char c in input)
+            {
+                if (c == 'x')
+                {
+                    xCount++;
+                }
+            }
+            foreach (char c in input)
+            {
+                if (c == 'x')
+                {
+                    oCount++;
+                }
+            }
+            return (oCount == xCount);
+        }
+    }
+}
